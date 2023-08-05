01f421253a82b246361fae96edcb4a5f59bf4082
alejandro.perez.acosta@hotmail.com <alejandro.perez.acosta@hotmail.com>
2023-08-04 23:01:32 -0500

    Aplicativo [MCSD - CONSULTAS] v[1.0.0.70] 1) Links to Soruce Code.

--- src/app/angular-tutorialsn-web/angular-tutorialsn-web.component.ts
+++ src/app/angular-tutorialsn-web/angular-tutorialsn-web.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component               } from '@angular/core';
+import { CustomErrorHandler      } from '../app.module';
+import { Observable,  throwError } from 'rxjs';
 //
 @Component({
   selector   : 'app-angular-tutorialsn-web',
@@ -14,4 +16,32 @@ export class AngularTutorialsnWebComponent {
   }
   //
   readonly pageTitle : string = AngularTutorialsnWebComponent.PageTitle;
+  //
+  constructor(private customErrorHandler : CustomErrorHandler)
+  {
+      //
+      console.log(AngularTutorialsnWebComponent.PageTitle + " - [INGRESO]") ;
+      //
+  }
+  //
+  TestError():void
+  {
+      //
+      let obs! : Observable<any>;
+      obs      = throwError("[ERROR THROWN TEST]");
+      //
+      obs.subscribe(
+        (        el: string) => {
+          console.log("Value Received :" + el);
+        },
+        (        err: string) => {
+          console.log("[ERROR CAUGTH TEST ] : " + err);
+        },
+        () => console.log("Processing Complete")
+      );
+      // CAUSAR ERROR
+      // @ts-ignore 
+      test = test+1;  
+  }
 }
+
