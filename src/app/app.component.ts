76b002e81542820842b8453b733146d4249cea7d
alejandro.perez.acosta@hotmail.com <aperezNWO>
2023-12-29 01:34:11 -0500

    Aplicativo [MCSD - CONSULTAS] v[1.0.1.15] 1) [GENERAR ARCHIVOS - XLS]. a) Date Time Pipe.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ export class AppComponent {
     public static title             : ( string | undefined ) = "[WEB API - DEV_ENV]"; 
     // propiedades internas
     public static appName           : ( string | undefined ) = "[WEB API DEV_ENV]";
-    public static appVersion        : ( string | undefined ) = "[1.0.1.14]";
+    public static appVersion        : ( string | undefined ) = "[1.0.1.15]";
     // propiedades publicas
     public readonly _title                                       : string | undefined  = AppComponent.title;
     public readonly _appName                                     : string | undefined  = AppComponent.appName;
